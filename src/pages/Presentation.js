cb5ed5a209b29f124292e30b099c2cf109af2885
Leonardo Federmann <leofedermann@gmail.com>
2021-03-31 14:25:38 -0300

    alterei a disposição dos botões

--- src/pages/Presentation.js
+++ src/pages/Presentation.js
@@ -29,7 +29,7 @@ const Main = styled.main`
 display:flex;
 flex-direction: column;
 align-items: center;
-justify-content: space-between;
+justify-content: space-around;
 height: 100vh;
 padding: 10px 0;
 font-family: 'Arial';
